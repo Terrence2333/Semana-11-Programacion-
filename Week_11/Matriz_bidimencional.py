63b67fefdb184c1731ef63babd0c2d2c0f85e989
terrence <terrencesthephens@gmail.com>
2025-03-11 01:04:03 -0500

    tarea semana #11

--- Week_11/Matriz_bidimencional.py
+++ Week_11/Matriz_bidimencional.py
@@ -1,23 +1,27 @@
-# Programa 1: Busqueda en Matriz Bidimencional
+
+# Programa 1: Busqueda en Matriz bidimencional
 
 def buscar_valor(matriz,valor):
 
- for i in range(0,len(matriz)):
-    for j in range(0,len(matriz[i])):
+ for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
         if matriz[i][j] == valor:
             return matriz[i][j]
         return i,j #return la posicion si lo encuentra
     return None #return name si no esta
  # Matriz 3x3
-Matriz = [[10,20,30],[40,50,60],[70,80,90]]
+Matriz = [[10,20,30],
+          [40,50,60],
+          [70,80,90]
+          ]
 
 # solicita valor a buscar
 
-buscar = buscar_valor(Matriz,10)
+buscar = buscar_valor(Matriz,valor=10)
 # resultado = buscar_valor(matriz,valor_buscar)
 if buscar:
+    resultado = 42
+    print(resultado)
 
-
- print(f'valor encontrado en la posicion: {buscar[ressultado]}')
 else:
     print('No se encontra la posicion en la matriz')
