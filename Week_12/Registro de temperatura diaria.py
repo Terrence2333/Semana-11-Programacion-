41f4255970d41982499a0548c122ae2ad5dabed3
terrence <terrencesthephens@gmail.com>
2025-03-13 13:07:26 -0500

    tarea semana #11

---
+++ Week_12/Registro de temperatura diaria.py
@@ -0,0 +1,49 @@
+# crear una matriz 3D para almacenar datos diarios de temperatura
+# primera dimension: Ciudades ( 3 ciudades)
+# segunda dimension: semanas ( 4 semanas)
+# tercera dimension: dias de la semana (7 dias )
+
+temperaturas = [22, 24, 21, 23, 25, 20, 19]
+umbral = 23
+
+#ciudad in "temperatura":
+#semana in "ciudad":
+
+
+# temepretura es =[
+
+ # Ciudad 1
+
+var = [  # Semana 1
+    [22, 24, 26, 28, 27, 23, 25],  # Días 1 a 7
+    # Semana 2
+    [21, 23, 25, 27, 26, 22, 24],
+    # Semana 3
+    [20, 22, 24, 26, 25, 21, 23],
+    # Semana 4
+    [19, 21, 23, 25, 24, 20, 22],
+]
+    # Ciudad 2
+v= [
+    # Semana 1
+    [30, 32, 31, 33, 34, 35, 36],
+    # Semana 2
+    [28, 29, 30, 31, 32, 33, 34],
+    # Semana 3
+    [27, 28, 29, 30, 31, 32, 33],
+    # Semana 4
+    [26, 27, 28, 29, 30, 31, 32],
+    ]
+ # Ciudad 3
+var: [
+    # Semana 1
+    [15, 17, 16, 18, 19, 20, 21],
+    # Semana 2
+    [14, 16, 17, 18, 19, 20, 21],
+    # Semana 3
+    [13, 15, 16, 17, 18, 19, 20],
+    # Semana 4
+    [12, 14, 15, 16, 17, 18, 19],
+]
+
+print("Días con temperatura superior a 23°C:")
