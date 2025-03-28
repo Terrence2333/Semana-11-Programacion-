751e8570a728bd499325a50d4b2578cd141de34b
terrence <terrencesthephens@gmail.com>
2025-03-27 20:34:46 -0500

    calcular_descuento_porcentaje_week_14

---
+++ Week_15/informacion_personal.py
@@ -0,0 +1,15 @@
+# datos personales
+
+
+
+datos_personales = {"terrence":{"edad":"43" "ciudad:" "cuenca" "profecion:""Ingeniero"}}
+
+
+datos_personales["telefono"] = "0987902593"
+datos_personales["ciudad"] = "CUENCA"
+
+
+
+del datos_personales["ciudad"]
+
+print(datos_personales["terrence"])
